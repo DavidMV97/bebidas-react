136c79fb74bdd682e64e8755bbcf57cbf9121f7e
Germán David <manchov12@gmail.com>
2024-06-12 19:43:35 -0500

    Main layout

---
+++ src/components/Header.tsx
@@ -0,0 +1,25 @@
+import { Link, NavLink } from "react-router-dom";
+
+export default function Header() {
+    return (
+        <header className="bg-slate-800" >
+            <div className="mx-auto container px-5 py-16">
+                <div className="flex justify-between items-center">
+                    <div>
+                        <img className="w-32" src="/logo.svg" alt="logotipo" />
+                    </div>
+                    <nav className="flex gap-4">
+                        <NavLink
+                            to="/"
+                            className={({ isActive }) => isActive ? "text-orange-500 font-bold uppercase" : "text-white font-bold uppercase"}>Inicio
+                        </NavLink>
+                        <NavLink
+                            to="/favoritos"
+                            className={({ isActive }) => isActive ? "text-orange-500 font-bold uppercase" : "text-white font-bold uppercase"}>Favoritos
+                        </NavLink>
+                    </nav>
+                </div>
+            </div>
+        </header>
+    )
+}
