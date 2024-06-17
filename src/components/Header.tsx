f2e194aab97eb54f26ceff4e12798892acd71d0a
Germán David <manchov12@gmail.com>
2024-06-17 14:22:01 -0500

    change background header home

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,14 +4,10 @@ import { NavLink, useLocation } from "react-router-dom";
 export default function Header() {
 
     const {pathname} = useLocation()
-
     const isHome = useMemo(() => pathname === '/' , [pathname])
-    
-    console.log(isHome);
-    
-
+        
     return (
-        <header className="bg-slate-800" >
+        <header className={ isHome ? 'bg-header bg-center bg-cover' : 'bg-slate-800'}>
             <div className="mx-auto container px-5 py-16">
                 <div className="flex justify-between items-center">
                     <div>
